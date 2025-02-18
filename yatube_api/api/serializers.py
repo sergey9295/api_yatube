a513490a45270e625856f5dd3d103adb198c4a6e
sergey9295 <133991719+sergey9295@users.noreply.github.com>
2025-02-18 13:18:21 +0700

    Add files via upload

---
+++ yatube_api/api/serializers.py
@@ -0,0 +1,65 @@
+from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
+from posts.models import Comment, Post, Group, Follow
+from django.contrib.auth import get_user_model
+from rest_framework.validators import UniqueTogetherValidator
+
+User = get_user_model()
+
+
+class PostSerializer(serializers.ModelSerializer):
+    group = serializers.PrimaryKeyRelatedField(
+        required=False,
+        queryset=Group.objects.all()
+    )
+    author = SlugRelatedField(slug_field='username', read_only=True)
+
+    class Meta:
+        fields = '__all__'
+        model = Post
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+    following = serializers.SlugRelatedField(
+        queryset=User.objects.all(),
+        slug_field='username'
+    )
+
+    class Meta:
+        model = Follow
+        fields = '__all__'
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following')
+            )
+        ]
+
+    def validate(self, data):
+        if data['following'] == self.context['request'].user:
+            raise serializers.ValidationError(
+                'Имя не может совпадать с цветом!')
+        return data
